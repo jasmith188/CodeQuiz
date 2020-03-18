08dd5ded3e3e183c36490591420010806bd86ebb
Jimmy Smith <jasmith.188@gmail.com>
2020-03-17 21:39:58 -0400

    main commit

---
+++ assets/js/questions.js
@@ -0,0 +1,13 @@
+questions = [
+    {
+        title: "example question 1:",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        answer: "Choice 3"
+    },
+    {
+        title: "example question 2:",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        answer: "Choice 3"
+    }
+    
+];
