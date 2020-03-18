c52511052e8c09bc087a5497963a8617854f0e02
Jimmy Smith <jasmith.188@gmail.com>
2020-03-17 22:50:05 -0400

    logic

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,22 +2,22 @@ questions = [
     {
         title: "Which NBA player is the All-Time leader in points scored?:",
         choices: ["Kobe Bryant", "Karl Malone", "Kareem Abdul-Jabbar", "Michael Jordan"],
-        answer: "Choice 3"
+        answer: "Kareem Abdul-Jabbar"
     },
     {
         title: "Which NBA Head Coach has one the most titles at 11?:",
         choices: ["Pat Riley", "Red Auerbach", "Greg Papovich", "Phil Jackson"],
-        answer: "Choice 4"
+        answer: "Phil Jackson"
     },
     {
         title: "Kobe Bryant was drafted by which team?:",
         choices: ["Charlotte Hornets", "Los Angeles Lakers", "Detroit Pistons", "New York Knicks"],
-        answer: "Choice 1"
+        answer: "Charlotte Hornets"
     },
     {
         title: "Which NBA team has one the most championships:",
         choices: ["Chicago Bulls", "Boston Celtics", "New York Knicks", "Los Angeles Lakers"],
-        answer: "Choice 2"
+        answer: "Boston Celtics"
     },
     
 
